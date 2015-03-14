0c0290a865f5e739c2c1a927710ca2464373e558
KlimMalgin <fxklim@gmail.com>
2015-03-14 16:44:45 +0500

    Добавил FileReaderMixin. Сделал тестовое чтение файла

--- app/handlers/ChooseFilesHandler.js
+++ app/handlers/ChooseFilesHandler.js
@@ -4,6 +4,18 @@
 
 QC.handlers.ChooseFilesHandler = (function () {
     return function (files) {
+        var ln = files.length,
+            r = null;
+
+        //for (var i = 0; i<ln; i++) {
+            r = QC.mixins.FileReaderMixin.createReader();
+            r.read(files[0])
+                .range(0, 55)
+                .done(function () {
+                    debugger;
+                }).go();
+        //}
+
         debugger;
     };
 })();
---
+++ app/mixins/FileReaderMixin.js
@@ -0,0 +1,55 @@
+/**
+ * Created by KlimMalgin on 14.03.2015.
+ */
+
+QC.mixins.FileReaderMixin = (function () {
+
+    function Reader() {
+        this.reader = null;
+        this.file = null;
+        this.blob = null;
+    }
+
+    Reader.prototype.read = function (file) {
+        this.file = file;
+        this.reader = new FileReader();
+        return this;
+    };
+
+    Reader.prototype.range = function (startByte, endByte) {
+        if (this.file.slice) {
+            this.blob = this.file.slice(startByte, endByte + 1);
+        } else
+        if (this.file.webkitSlice) {
+            this.blob = this.file.webkitSlice(startByte, endByte + 1);
+        } else
+        if (this.file.mozSlice) {
+            this.blob = this.file.mozSlice(startByte, endByte + 1);
+        }
+        return this;
+    };
+
+    Reader.prototype.done = function (callback) {
+        this.reader.onloadend = function(evt) {
+            if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+                callback.apply(this, arguments);
+            } else {
+                console.error('State is not DONE!');
+            }
+        };
+        return this;
+    };
+
+    Reader.prototype.go = function () {
+        this.reader.readAsBinaryString(this.blob);
+        return this;
+    };
+
+
+
+    return {
+        createReader:function () {
+            return new Reader();
+        }
+    };
+})();
