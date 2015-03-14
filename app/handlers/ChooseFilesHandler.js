21ba644bc89c26bd653ea46a83c762ce1e3fe3c0
KlimMalgin <fxklim@gmail.com>
2015-03-14 17:16:43 +0500

    Разбиение файла на строки и строк на элементы

--- app/handlers/ChooseFilesHandler.js
+++ app/handlers/ChooseFilesHandler.js
@@ -3,18 +3,30 @@
  */
 
 QC.handlers.ChooseFilesHandler = (function () {
+
+    function done(result) {
+
+    }
+
     return function (files) {
         var ln = files.length,
+            FileArray = [],
+            file = files[0],
+            size = file.size - 1,
             r = null;
 
-        //for (var i = 0; i<ln; i++) {
-            r = QC.mixins.FileReaderMixin.createReader();
-            r.read(files[0])
-                .range(0, 55)
-                .done(function () {
-                    debugger;
-                }).go();
-        //}
+        r = QC.mixins.FileReaderMixin.createReader().read(file);
+        r.range(0, size)
+            .done(function (e) {
+                FileArray = e.target.result.split('\n');
+                var faLn = FileArray.length;
+
+                for (var i = 0; i<faLn; i++) {
+                    FileArray[i] = FileArray[i].split(',');
+                }
+
+                debugger;
+            }).go();
 
         debugger;
     };
